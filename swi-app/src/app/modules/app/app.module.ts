cb654842467fc2c0be71c70e8d3470c216cd161e
Steve Kent <steve_kent@beaerospace.com>
2017-03-31 13:22:26 +0100

    Added basic router support
    SWI-22 #done

--- swi-app/src/app/modules/app/app.module.ts
+++ swi-app/src/app/modules/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { SwiBuilderModule } from '../swi-builder/swi-builder.module';
@@ -9,13 +10,22 @@ import { ElectronKioskService } from '../../services/electron-kiosk.service';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToastModule } from 'ng2-toastr';
 import { DemoComponent } from './demo/demo.component';
+import { HomeComponent } from './home/home.component';
+
+const appRoutes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch:'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'demo', component: DemoComponent },
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    DemoComponent
+    DemoComponent,
+    HomeComponent,
   ],
   imports: [
+    RouterModule.forRoot(appRoutes),
     BrowserModule,
     FormsModule,
     HttpModule,
