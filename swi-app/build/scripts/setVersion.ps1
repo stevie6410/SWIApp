9d2294dabe240513212b5f4c31d0a374d9dd995a
Steve Kent <steve_kent@beaerospace.com>
2017-04-25 09:54:49 +0100

    Update to version script

--- swi-app/build/scripts/setVersion.ps1
+++ swi-app/build/scripts/setVersion.ps1
@@ -1,11 +1,14 @@
 # These are project build parameters in TeamCity
 # Depending on the branch, we will use different major/minor versions
+param(
+    [string]$buildCounter
+)
 [System.Reflection.Assembly]::LoadWithPartialName("System.Web.Extensions")
 $ser = New-Object System.Web.Script.Serialization.JavaScriptSerializer
 $json = Get-Content "package.json" | Out-String
 $obj = $ser.DeserializeObject($json)
 $majorVerion = ($obj['version'] -split "\.")[0]
 $minorVerion = ($obj['version'] -split "\.")[1]
-$buildCounter = "%teamcity.build.counter%" 
+# $buildCounter = "%teamcity.build.counter%" 
 $buildNumber = "$majorVerion.$minorVerion.$buildCounter"
 Write-Host "##teamcity[buildNumber '$buildNumber']"
