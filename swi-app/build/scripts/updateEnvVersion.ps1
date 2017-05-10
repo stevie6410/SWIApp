8d3a62463a0f0e3d19777b738128da1edc5c339d
Steve Kent <steve_kent@beaerospace.com>
2017-05-10 14:31:53 +0100

    Added -Raw flags to powershell scripts

--- swi-app/build/scripts/updateEnvVersion.ps1
+++ swi-app/build/scripts/updateEnvVersion.ps1
@@ -1,5 +1,5 @@
-$package = Get-Content "package.json" | ConvertFrom-Json
-$env = Get-Content "environment.json" | ConvertFrom-Json
+$package = Get-Content "package.json" -Raw | ConvertFrom-Json
+$env = Get-Content "environment.json" -Raw | ConvertFrom-Json
 
 $env.version = $package.version
 $env | ConvertTo-Json | Set-Content "environment.json"
