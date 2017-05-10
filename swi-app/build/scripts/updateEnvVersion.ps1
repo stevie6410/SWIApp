a45a5cddb2ae46c80f53e27c60a0599ad7686276
Steve Kent <steve_kent@beaerospace.com>
2017-05-10 14:22:16 +0100

    Updated EnvScript

--- swi-app/build/scripts/updateEnvVersion.ps1
+++ swi-app/build/scripts/updateEnvVersion.ps1
@@ -1,8 +1,5 @@
-# $ser = New-Object System.Web.Script.Serialization.JavaScriptSerializer
-# $json = Get-Content "package.json" | Out-String
-$obj = Get-Content "package.json" | ConvertFrom-Json
-$version = $obj['version']
-
+$package = Get-Content "package.json" | ConvertFrom-Json
 $env = Get-Content "environment.json" | ConvertFrom-Json
-$env.version = $version
+
+$env.version = $package.version
 $env | ConvertTo-Json | Set-Content "environment.json"
