b238a52704dd11ffde642334e2697c2d70cd1ed0
Steve Kent <steve_kent@beaerospace.com>
2017-05-15 12:39:19 +0100

    Added scaffolded camera module and screen component
    SWI-114

---
+++ swi-app/src/app/modules/camera/camera.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CameraScreenComponent } from './components/camera-screen/camera-screen.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [CameraScreenComponent]
+})
+export class CameraModule { }
---
+++ swi-app/src/app/modules/camera/components/camera-screen/camera-screen.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'swi-camera-screen',
+  templateUrl: './camera-screen.component.html',
+  styleUrls: ['./camera-screen.component.scss']
+})
+export class CameraScreenComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
