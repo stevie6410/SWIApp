4661638351548917f8022b1e6209bcbf54ac9b00
Steve Kent <steve_kent@beaerospace.com>
2017-06-15 15:35:32 +0100

    Added database versioning

--- swi-app/src/app/modules/core/swi-db.service.ts
+++ swi-app/src/app/modules/core/swi-db.service.ts
@@ -3,7 +3,13 @@ import Dexie from 'dexie';
 export class SWIDBService extends Dexie {
     constructor() {
         super('swidb');
+
         this.version(1).stores({
+            swis: 'id',
+            appConfig: 'id++',
+        });
+
+        this.version(2).stores({
             swis: 'id',
             appConfig: 'id++',
             imageStore: 'key'
