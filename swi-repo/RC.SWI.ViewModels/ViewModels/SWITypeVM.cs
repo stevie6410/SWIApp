005f91311ab414aa8cc573c07f2dbf0d61dd4a54
Steve Kent <steve_kent@beaerospace.com>
2017-06-20 19:10:23 +0100

    Added Controller for AppCatalog
    SWI-178 #done

--- swi-repo/RC.SWI.ViewModels/ViewModels/SWITypeVM.cs
+++ swi-repo/RC.SWI.ViewModels/ViewModels/SWITypeVM.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RC.SWI.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,5 +12,12 @@ namespace RC.SWI.ViewModels.ViewModels
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
+
+        public SWITypeVM(SWIType type)
+        {
+            Id = type.Id;
+            Name = type.Name;
+            Description = type.Description;
+        }
     }
 }
