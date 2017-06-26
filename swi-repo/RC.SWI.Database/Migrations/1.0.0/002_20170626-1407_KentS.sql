dc695b282970feca77e371a8f08ac7154a55b1f1
Steve Kent <steve_kent@beaerospace.com>
2017-06-26 14:10:58 +0100

    Added back in Database project

---
+++ swi-repo/RC.SWI.Database/Migrations/1.0.0/002_20170626-1407_KentS.sql
@@ -0,0 +1,8 @@
+﻿-- <Migration ID="71521908-ba4a-43bf-9fb1-c3423717bb9a" />
+GO
+PRINT N'Altering [swi].[StandardTools]'
+GO
+ALTER TABLE [swi].[StandardTools] ALTER COLUMN [Image] [varchar] (max) NULL
+GO
+ALTER TABLE [swi].[StandardTools] ALTER COLUMN [HasCarePoint] [bit] NULL
+GO
