0b126dec6cc80f6a4585809f9b90332c5161fbc0
Steve Kent <steve_kent@beaerospace.com>
2017-08-07 12:36:34 +0100

    Confirmation dialog when deleting image
    SWI-253 #done

--- swi-app/src/app/camera/components/camera-control/camera-control.component.ts
+++ swi-app/src/app/camera/components/camera-control/camera-control.component.ts
@@ -1,29 +1,31 @@
-import { Component, OnInit, Input, Output, EventEmitter, ChangeDetectorRef, ViewChild } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, ChangeDetectorRef, ViewChild, AfterViewInit } from '@angular/core';
 import { CameraService } from "../../services/camera.service";
 import { CaptureImage } from "../../models/capture-image";
 import { ImageCropperComponent } from "../camera-cropper/image-cropper.component";
 import { GUID } from "app/core";
+import { Modal } from "angular2-modal/plugins/bootstrap";
 
 @Component({
   selector: 'swi-camera',
   templateUrl: './camera-control.component.html',
   styleUrls: ['./camera-control.component.scss']
 })
-export class CameraControlComponent implements OnInit {
+export class CameraControlComponent implements OnInit, AfterViewInit {
 
   @Input() captureImage: CaptureImage;
   @Output() onCaptured = new EventEmitter<void>();
   @Output() onCanceled = new EventEmitter<void>();
   @ViewChild("cropper") cropper: ImageCropperComponent;
 
-  isCroppingMode: boolean = false;
-  isCaptureMode: boolean = false;
-  isFileMode: boolean = false;
+  isCroppingMode = false;
+  isCaptureMode = false;
+  isFileMode = false;
   image: string;
 
   constructor(
     private cameraService: CameraService,
-    private changeDetector: ChangeDetectorRef
+    private changeDetector: ChangeDetectorRef,
+    private modal: Modal
   ) { }
 
   ngOnInit() {
@@ -50,18 +52,36 @@ export class CameraControlComponent implements OnInit {
   }
 
   deleteImage() {
-    let captureImage = new CaptureImage();
-    // captureImage.image = null;
-    // captureImage.key = new GUID().value;
-    this.image = null;
-    this.captureImage.image = null;
-    this.captureImage.key = new GUID().value;
 
-    this.cameraService.cameraCompleted(this.captureImage);
-    this.onCaptured.emit();
+    this.modal.confirm()
+      .size('lg')
+      .isBlocking(true)
+      .showClose(false)
+      .keyboard(27)
+      .titleHtml('<h5>Confirm Delete Image</h5>')
+      .body(`Are you sure you want to delete image?`)
+      .okBtn('Delete Image')
+      .okBtnClass('btn btn-danger')
+      .cancelBtn('Cancel')
+      .cancelBtnClass('btn btn-secondary')
+      .open()
+      .then(dialogRef => dialogRef.result)
+      .then(result => {
+
+        // Execute delete logic
+
+        const captureImage = new CaptureImage();
+        this.image = null;
+        this.captureImage.image = null;
+        this.captureImage.key = new GUID().value;
+
+        this.cameraService.cameraCompleted(this.captureImage);
+        this.onCaptured.emit();
+      })
+      .catch(err => console.log("Canceled delete image"));
   }
 
-  //#####################################
+  // #####################################
 
   setImage(image: string, crop: boolean) {
     this.image = image;
