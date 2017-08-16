f9253db96997c579bd6566fa624aa3a52a0df5be
Steve Kent <steve_kent@beaerospace.com>
2017-08-16 15:10:10 +0100

    Recreated database scripts

--- swi-repo/RC.SWI.Database/Migrations/1.0.0/004_20170629-1138_KentS.sql
+++
@@ -1,8 +0,0 @@
-﻿-- <Migration ID="b7b51710-4844-43e1-942f-dea2878a4dd6" />
-GO
-
-PRINT N'Altering [dbo].[HealthAndSafetyIcon]'
-GO
-ALTER TABLE [dbo].[HealthAndSafetyIcon] ADD
-[Caption] [nvarchar] (255) NULL
-GO
